9b28bc1839bfb36f8fd17ced07e7978a80faf4fe
Eduardo Gonzalez <eduardgp@gmail.com>
2019-08-22 16:47:14 -0400

    added loading

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Room from './components/room/room';
+
+function App() {
+	return <Room />;
+}
+
+export default App;
